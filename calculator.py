cf6d559a33f12938f1fc04c69f03f2836090bc66
Gudmundur Kristjan <gummigudna@gmail.com>
2021-09-10 20:25:53 +0000

    einhver böggur

--- calculator.py
+++ calculator.py
@@ -1,1 +1,30 @@
- 
+ """
+ Notandi slær endurtekið inn reiknisegð
+    Reiknisegð = (int) (operator) (int)
+þangað til slegið er q
+    while x != "q"
+
+Niðurstaða skrifuð út með 2 aukastöfum
+Leyfilegir virkjar/operatorar:
+    +,-,*,/
+Bara int tölur
+Ef deiling þá má seinni int ekki vera 0
+
+Skoða villuboð
+"""
+#Biðja um streng
+    #Biðja um int
+    #Biðja um reiknisegð
+    #Biðja um int
+
+first_int = int(input("Enter an equation:" ))
+
+#first_split = first_int.split(' ')
+
+#print (first_split)
+
+#Framkvæma reikniaðgerðir
+
+#Villucheck/Villuboðar
+
+
