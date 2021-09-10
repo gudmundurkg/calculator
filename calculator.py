f240facfa5008912870fc43e9ffe8bcf7e695977
Gudmundur Kristjan <gummigudna@gmail.com>
2021-09-10 22:24:42 +0000

    Added error if input is not numerical

--- calculator.py
+++ calculator.py
@@ -17,7 +17,7 @@ Skoða villuboð
     #Biðja um reiknisegð
     #Biðja um int
 
-user_str = input("Enter an equation:" )
+user_str = input("Enter an equation: " )
 
 
 while user_str != "q":
@@ -28,32 +28,41 @@ while user_str != "q":
     second_str = user_str_split[2]
     operator = user_str_split[1]
 
+    #Skoðum hvort inntak sé bókstafur
+    numeric_check_first = first_str.isnumeric()
+    numeric_check_second = second_str.isnumeric()
+    
+    if numeric_check_first == False or numeric_check_second == False:
+        print("Invalid operands")
+        user_str = input("Enter an equation: " )
+        continue
     #Umbreytum str inputti í int
     first_int = int(first_str)
     second_int = int(second_str)
 
+    
     if operator != "+" and operator != "-" and operator != "*" and operator != "/":
         print("Invalid operator")
     elif operator == "+": #Samlagning
         sum = (first_int + second_int)
         sum_float = "{:.2f}".format(sum)
-        print(sum_float)
+        print("Result:", sum_float)
     elif operator == "-": #Frádráttur
         subtract = first_int - second_int
         subtract_float = "{:.2f}".format(subtract)
-        print(subtract_float)
+        print("Result", subtract_float)
     elif operator == "*": #Margföldun
         multiply = first_int * second_int
         multiply_float = "{:.2f}".format(multiply)
-        print(multiply_float)
+        print("Result", multiply_float)
     elif operator == "/": #Deiling
         if second_int == 0:
             print("Cant divide by 0")
         else:
             divide = first_int / second_int
             divide_float = "{:.2f}".format(divide)
-            print(divide_float)
-    user_str = input("Enter an equation:" )
+            print("Result", divide_float)
+    user_str = input("Enter an equation: " )
 
 
 #Villucheck/Villuboða
